f6cf20e1f4092de2f137d1e5b46856cda4e0d6ec
graphway <51998512+graphway@users.noreply.github.com>
2019-08-30 09:16:14 +1000

    Add files via upload

---
+++ 培训/图数据库及图算法/procedures/src/main/java/com/mypackage/results/StringResult.java
@@ -0,0 +1,10 @@
+package com.mypackage.results;
+
+public class StringResult {
+    public final static StringResult EMPTY = new StringResult(null);
+    public final String value;
+
+    public StringResult(String value) {
+        this.value = value;
+    }
+}
